a42abd4111d7dc79f07df5e0c72be078673a87dd
ypgajjar <yashpal.pgajjar@gmail.com>
2025-04-22 21:06:10 +0000

    fix: set correct base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,9 @@
 // vite.config.js
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: './',
   plugins: [react()],
-  build: { outDir: 'docs' }
-})
-
+  /* 👇 add or change this line */
+  base: "/qa-qc-checklists/",   // <- the name of the repo with trailing slash
+});

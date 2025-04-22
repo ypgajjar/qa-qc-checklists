fbe64eec3c0c26c90935b36a4dfbe680e1c1f232
ypgajjar <yashpal.pgajjar@gmail.com>
2025-04-22 00:33:10 +0000

    chore: build into docs/ for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,11 @@
 // vite.config.js
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: "/qa-qc-checklists/",   // your repo name
-  plugins: [react()],
+  base: '/qa‑qc‑checklists/',
+  plugins: [ react() ],
   build: {
-    outDir: "docs"
+    outDir: 'docs'
   }
-});
+})

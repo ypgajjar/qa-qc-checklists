fbc30ec848d4cd4ef3525f4e6f8e2cf23bec1cd3
ypgajjar <yashpal.pgajjar@gmail.com>
2025-04-22 00:40:31 +0000

    chore: fix base path for GH Pages

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/qa‑qc‑checklists/',
-  plugins: [ react() ],
-  build: {
-    outDir: 'docs'
-  }
+  base: './',
+  plugins: [react()],
+  build: { outDir: 'docs' }
 })
+

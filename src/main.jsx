0e5fe3bdd9c721281deaa8466950807885516ff0
ypgajjar <yashpal.pgajjar@gmail.com>
2025-04-23 00:23:31 +0000

    Fix base path and remove Router basename

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,8 @@ import App from './App'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter basename={import.meta.env.BASE_URL}>
-    <App />
+  <BrowserRouter>
+  <App />
   </BrowserRouter>
+
 )
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+// vite.config.js
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   base: '/qa-qc-checklists/',
   plugins: [react()],
-});
+})
